60c992e9eb2b85e0e1dedc168085f81313ae441b
Newton Lomar <newtonlomar16@gmail.com>
2022-12-23 02:17:42 -0300

    criando arquivo json

--- modules/escrever.js
+++ modules/escrever.js
@@ -1,8 +1,25 @@
 const fs = require("fs").promises;
 const path = require("path");
-const caminhoArquivo = path.resolve(__dirname, "..", "teste.txt");
+const caminhoArquivo = path.resolve(__dirname, "..", "teste.json");
 
-fs.writeFile(caminhoArquivo, "Frase 1\n", {
+const pessoas = [
+  {
+    nome: "Newton",
+  },
+  {
+    nome: "Matheus",
+  },
+  {
+    nome: "Ana",
+  },
+  {
+    nome: "Beatriz",
+  },
+];
+
+const json = JSON.stringify(pessoas);
+
+fs.writeFile(caminhoArquivo, json, {
   flag: "a",
   //encoding: "utf8",
 });
