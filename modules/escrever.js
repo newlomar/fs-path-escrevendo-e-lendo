dee9ef490d4124498fb022e6be402a56f648172e
Newton Lomar <newtonlomar16@gmail.com>
2022-12-23 02:14:45 -0300

    changing to append

--- modules/escrever.js
+++ modules/escrever.js
@@ -2,7 +2,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const caminhoArquivo = path.resolve(__dirname, "..", "teste.txt");
 
-fs.writeFile(caminhoArquivo, "Frase 1", {
-  flag: "w",
+fs.writeFile(caminhoArquivo, "Frase 1\n", {
+  flag: "a",
   //encoding: "utf8",
 });
